db97000c83ba31ada592b98a1603ff04868e9315
Kiarahmani <rahmank@purdue.edu>
2020-07-20 13:05:07 -0400

    edit

--- ms_src/create_bench_files.py
+++ ms_src/create_bench_files.py
@@ -12,9 +12,7 @@ input_file =  'ms_src/curated_benchmark_csv.csv'
 output_dir =  'ms_src/benchmarks/'
 results_dir = 'ms_src/results/'
 file_prefix = 'msb'
-
-
-
+cegis       = True
 
 def run(cmd, timeout_sec, f):
     proc = Popen(shlex.split(cmd), stdout=PIPE, stderr=PIPE)
@@ -28,7 +26,7 @@ def run(cmd, timeout_sec, f):
         timer.cancel()
 
 
-def create_ms_bench():
+def create_bench():
     dt = pd.read_csv (input_file, encoding="iso-8859-1")
     iter = 0
     for row in dt.iterrows():
@@ -36,11 +34,15 @@ def create_ms_bench():
         output_file = output_dir + file_prefix + "_" + str(iter)
         # read required columns
         q_regex = row[1].Q_regex
+        a_regex = row[1].A_regex
         examples = row[1].postivie_examples
         with open(output_file, 'w', encoding="utf-8") as of:
             print (">> writing "+output_file)
             of.write(q_regex)
             of.write("\n")
+            if cegis:
+                of.write(a_regex)
+                of.write("\n")
             of.write("+++")
             of.write("\n")
             # iterate over examples 
@@ -61,7 +63,6 @@ def run_tests():
             continue
         result_file = results_dir + bench_name + '.res'
         with open(result_file, 'w', encoding='utf-8') as rf:
-            
             run ("java -jar target/regfixer.jar fix --file "+file, 10, rf)
             #try:
             #    #subprocess.run(["java", "-jar" , "target/regfixer.jar", "fix" , "--file", file], stdout=rf)
@@ -70,8 +71,8 @@ def run_tests():
             #    rf.write("Error occured")
 
 def main():
-    create_ms_bench()
-    run_tests()
+    create_bench()
+    #run_tests()
 
 
 
